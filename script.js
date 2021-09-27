1bdc65938bb6a95946d5976cf6d45c74d1042489
Code Sandbox <hello@codesandbox.io>
2021-09-27 15:44:36 +0000

    Deployment from Codesandbox.

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("before");
+setTimeout(function () {
+  var nodes = document.querySelectorAll("iframe");
+  var last = nodes[nodes.length - 1];
+  //last.style.display = "none";
+
+  last.style.border = "1px solid red";
+}, 2000);
